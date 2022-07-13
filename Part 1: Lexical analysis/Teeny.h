6605334b02aa6f4a597c012f26acce5ca1da9656
borismix <borys.mykhalevych@web.de>
2022-07-14 00:44:58 +0200

    Update Teeny.h

--- Part 1: Lexical analysis/Teeny.h
+++ Part 1: Lexical analysis/Teeny.h
@@ -1,1 +1,11 @@
+/******************************************************************************/
+/*                                                                            */
+/*    Copyright © 2022 SemWare Corporation. All rights reserved worldwide.    */
+/*                                                                            */
+/******************************************************************************/
 
+#ifndef TEENY_HEADER_H
+#define TEENY_HEADER_H
+
+
+#endif  TEENY_HEADER_H

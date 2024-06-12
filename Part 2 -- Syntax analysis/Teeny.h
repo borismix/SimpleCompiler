b38650b69f9df829ba52d3d5dd0d22274be67a76
myk <borys.mykhalevych@sprecher-automation.com>
2024-06-12 19:34:00 +0200

    Created new Solution and Project. Copied Source Code from previous Stage.

--- Part 2 -- Syntax analysis/Teeny.h
+++ Part 2 -- Syntax analysis/Teeny.h
@@ -8,5 +8,36 @@
 #ifndef TEENY_HEADER_H
 #define TEENY_HEADER_H
 
+enum { Ident_max = 16 };
+
+typedef char Ident[ Ident_max + 1 ];
+
+typedef enum
+{
+  null, times, plus, rparen, lparen, number, writesym, eofsym
+} Symbol;
+
+class Scanner
+{
+public:
+  bool init( const char fn[] );
+  Symbol getsym( void );
+  int getval( void ) { return val; }
+  const char *getid( void ) { return id; }
+
+private:
+  void read_ch( void );
+  Symbol get_ident( void );
+  Symbol get_number( void );
+  void enter_kw( Symbol sym, const char* name );
+  enum { KW = 25 };
+  int val;
+  Ident id;
+  int ch;
+  int nkw;
+  const char*  keyTab[ KW + 1 ];
+  Symbol keySym[ KW + 1 ];
+  FILE* file;
+};
 
 #endif  TEENY_HEADER_H
